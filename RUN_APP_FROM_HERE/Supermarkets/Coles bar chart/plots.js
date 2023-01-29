96332f42a5e02f9a406c60192c759ba3c94dc840
shanCH3n <111955977+shanCH3n@users.noreply.github.com>
2023-01-29 18:34:15 +0800

    fuel map displays when dashboard is run from a flask

---
+++ RUN_APP_FROM_HERE/Supermarkets/Coles bar chart/plots.js
@@ -0,0 +1,38 @@
+// Sort the data by Greek search results descending
+let sortedByprice = data.sort((a, b) => b.price - a.price);
+
+// Slice the first 10 objects for plotting
+slicedData = sortedByprice.slice(0, 20);
+
+// Reverse the array to accommodate Plotly's defaults
+reversedData = slicedData.reverse();
+
+// Trace1 for the Greek Data
+let trace1 = {
+  x: reversedData.map(object => object.price),
+  y: reversedData.map(object => object.title),
+  text: reversedData.map(object => object.price),
+  name: "Coles",
+  type: "bar",
+  orientation: "h"
+  
+};
+
+// Data array
+// `data` has already been defined, so we must choose a new name here:
+let traceData = [trace1];
+
+// Apply a title to the layout
+let layout = {
+  title: "Coles Cofee Prices",
+  margin: {
+    l: 600,
+    r: 80,
+    t: 50,
+    b: 80
+  }
+};
+
+// Render the plot to the div tag with id "plot"
+// Note that we use `traceData` here, not `data`
+Plotly.newPlot("plot", traceData, layout);
