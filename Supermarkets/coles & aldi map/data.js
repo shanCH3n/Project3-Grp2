7ce3cfec8a6c8e2c8cab2a67189db03bdbc80869
rubab-malik <113024685+rubab-malik@users.noreply.github.com>
2023-01-30 16:35:53 +0800

    Add files via upload

---
+++ Supermarkets/coles & aldi map/data.js
@@ -0,0 +1,112 @@
+const data=[
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates":  [-31.74136, 115.73905]
+        },
+        "properties": {
+            "price": "$2",
+            "title": "AVERAGE PRICE: Bread Coles",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [ -31.87691, 115.77889]
+        },
+        "properties": {
+            "price": "$3.47",
+            "title": "AVERAGE PRICE: Flavoured Milk | 1L",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [ -31.940944, 115.811989] 
+        },
+        "properties": {
+            "price": "$5.44",
+            "title": "AVERAGE PRICE: Jams, Honey & Spreads",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-31.984976, 115.900803] 
+        },
+        "properties": {
+            "price": "$16.5",
+            "title": "AVERAGE PRICE: Lamb in meat",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-31.74427,115.76657]
+        },
+        "properties": {
+            "price": "$1.37",
+            "title": "AVERAGE PRICE: Choclates-Bar",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-31.9648692573, 115.93465175] 
+        },
+        "properties": {
+            "price": "$4.56",
+            "title": "AVERAGE PRICE: Cofee Capsules for 10pk",
+            "Store":"COLES"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates":  [-31.9513993, 115.857376]
+        },
+        "properties": {
+            "price": "$2.0",
+            "title": "AVERAGE PRICE: Milk  | 1L",
+            "Store":"   Aldi"
+    }
+    },
+    {
+        "type": "Feature",
+        "geometry": { 
+            "type": "Point", 
+            "coordinates": [-32.0210441,115.9397830]
+         
+        },
+        "properties": {
+            "price": "$4.4",
+            "title": "AVERAGE PRICE: Cofee Capsules for 10pk",
+            "Store":"   Aldi"
+    }
+    },
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
---
+++ Supermarkets/coles & aldi map/logic-step1.js
@@ -0,0 +1,86 @@
+
+
+// Creating the map object
+const map= L.map("map").setView([-31.953512, 115.857048],11) ;
+  
+
+// Adding the tile layer
+const titleUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tiles=L.tileLayer(titleUrl,{attribution});
+
+tiles.addTo(map);
+
+
+function generateList(){
+    const ul=document.querySelector('.list');
+    data.forEach((item) => {
+        const li=document.createElement('li');
+        const div =document.createElement('div');
+        const a=document.createElement('a');
+        const p=document.createElement('p');
+        const r=document.createElement('p');
+        
+        a.addEventListener('Click',() =>{
+          flytostore(shop);
+        })
+
+        div.classList.add('shop_items');
+        a.innerText=item.properties.title;
+        a.href='#';
+        p.innerText=item.properties.price;
+        r.innerText=item.properties.Store;
+        
+
+
+        div.appendChild(a);
+        div.appendChild(p);
+        div.appendChild(r);
+
+        li.appendChild(div);
+        ul.appendChild(li);
+        
+    
+
+
+    });
+    
+}
+
+generateList();
+
+
+
+
+function makepopupContent(shop){
+    return `<div> 
+    <h4>${shop.properties.title}</h4>
+    <p>${shop.properties.price}</p>
+    <r>${shop.properties.Store}</r>
+    </div>`;
+}
+
+
+function onEachFeature(feature,layer){
+    layer.bindPopup(makePopupContent(feature));
+
+}
+
+
+const markers = new L.LayerGroup();
+
+for (let i = 0; i < data.length; i++) {
+    const feature = data[i];
+    const latlng = [feature.geometry.coordinates[0], feature.geometry.coordinates[1]];
+    const marker = L.marker(latlng);
+    marker.bindPopup(makepopupContent(feature));
+    markers.addLayer(marker);
+}
+
+
+markers.addTo(map);
+
+function flytostore(Store){
+  map.flyTo(Store.geometry.coordinates[1],store.geometry.coordinates[0])
+  
+}
